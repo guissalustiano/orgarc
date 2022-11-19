b70b846aa43cd017538ceaaf73cbbc298b7ba603
Guilherme Salustiano <guissalustiano@gmail.com>
2022-11-19 20:58:38 -0300

    load rom from file

--- ex4a/processor/rom.vhdl
+++ ex4a/processor/rom.vhdl
@@ -1,33 +1,44 @@
 library ieee;
 use ieee.numeric_bit.all;
+use std.textio.all;
 
 entity rom is
+   generic(
+     file_name: string := "rom.dat";
+     address_size : natural := 32;
+     word_size    : natural := 32
+   );
    port (
-      -- 32 bits de endereço:
-      addr: in bit_vector(31 downto 0);
-      -- 32 bits de tamanho de palavra de dados:
-      data: out bit_vector(31 downto 0)
+      address   : in  bit_vector(address_size-1 downto 0);
+      read_data : out bit_vector(word_size-1 downto 0)
    );
 end rom;
 
-architecture rom_arch of rom is
-  type mem_tipo is array (0 to 2**32-1) of bit_vector(31 downto 0);
-  constant mem: mem_tipo := (
-       0 => "11111000010000000000001111100000", -- LDUR X0, [X31, 0]
-       1 => "11111000010000001000001111100001", -- LDUR X1, [X31, 8]
-       2 => "11111000010000010000001111100010", -- LDUR X2, [X31, 16]
-       3 => "11111000010000011000001111100011", -- LDUR X3, [X31, 24]
-       4 => "10001011000000000000000000100101", -- ADD X5, X1, X0
-       5 => "10001011000111110000000000100000", -- ADD X0, X1, X31
-       6 => "10001011000111110000000010100001", -- ADD X1, X5, X31
-       7 => "11001011000000110000000001000010", -- SUB X2, X2, X3
-       8 => "10110100000000000000000001000010", -- CBZ X2, 2
-       9 => "00010111111111111111111111111011", -- B -5
-      10 => "11111000000000100000001111100001", -- STUR X1, [X31, 32]
-      11 => "00010100000000000000000000000000", -- B 0
-       others => x"00000000"
-    );
+architecture de_arquivo of rom is
+  constant depth : natural := 2**address_size;
+  type mem_t is array (0 to depth-1) of bit_vector(word_size-1 downto 0);
 
+  --! Funcao que le o arquivo e preenche uma matriz temporaria
+  --! Saida: matriz identica ao mem_t, preenchida com os valores do arquivo
+  impure function inicializa(nome_do_arquivo : in string) return mem_t is
+    file     arquivo  : text open read_mode is file_name;
+    variable linha    : line;
+    variable temp_bv  : bit_vector(3 downto 0);
+    variable temp_mem : mem_t;
+    begin
+      --! Itera sobre o tamanho do tipo de dados (profundidade da matriz)
+      for i in mem_t'range loop
+        --! Le uma linha inteira do arquivo e coloca em "linha"
+        readline(arquivo, linha);
+        --! Interpreta a linha com um unico bit_vector e coloca em "temp_bv"
+        read(linha, temp_bv);
+        --! Atribui o valor lido para a matriz
+        temp_mem(i) := temp_bv;
+      end loop;
+      return temp_mem;
+    end;
+  --! Delaracao da matriz de memoria em si, note que foi chamada a funcao para preenche-la.  
+  constant mem : mem_t := inicializa();
 begin
-   data <= mem(to_integer(unsigned(addr)));
-end rom_arch;
+  data <= mem(to_integer(unsigned(addr)));
+end de_arquivo;
